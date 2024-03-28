81ff1e3510751fb178ca53de327e2d3ae846550a
donovanodom <dodomdev@gmail.com>
2024-03-28 09:51:31 -0700

    Time: 119 ms (5.37%) | Memory: 60.9 MB (5.37%) - LeetSync

---
+++ 764-n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.js
@@ -0,0 +1,18 @@
+const levelOrder = (root) => {
+    if(!root) return []
+    let q = [[root,0]], map = {}
+    while(q.length){
+        for(const [node,x] of q){
+            if(!map[x]) map[x] = []
+            map[x].push(node.val)
+        }
+        const len = q.length
+        let i = 0
+        while(i < len){
+            let [node,x] = q.shift()
+            node.children.map((child) => q.push([child,x+1]))
+            i++
+        }
+    }
+    return Object.values(map).map((val) => val)
+}
