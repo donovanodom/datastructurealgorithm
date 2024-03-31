a8077bbac1520f8288674007363590aab6cb29ab
donovanodom <dodomdev@gmail.com>
2024-03-31 10:10:42 -0700

    Time: 124 ms (5.44%), Space: 66.1 MB (5.44%) - LeetHub

--- 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.js
+++ 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.js
@@ -1,18 +1,26 @@
-const levelOrder = (root) => {
+const levelOrder = (root, level = 0, map = {}) => {
   if(!root) return []
-  const q = [[root,0]], map = {}
-  while(q.length){
-    for(const [node,x] of q){
-      if(!map[x]) map[x] = []
-      map[x].push(node.val)
-    }
-    const len = q.length
-    let i = 0
-    while(i < len){
-      let [node,x] = q.shift()
-      node.children.map(child => q.push([child,x+1]))
-      i++
-    }
-  }
+  if(!map[level]) map[level] = []
+  map[level].push(root.val)
+  root.children.map(child => levelOrder(child, level + 1, map))
   return Object.values(map).map(val => val)
 }
+
+// const levelOrder = (root) => {
+//   if(!root) return []
+//   const q = [[root,0]], map = {}
+//   while(q.length){
+//     for(const [node,x] of q){
+//       if(!map[x]) map[x] = []
+//       map[x].push(node.val)
+//     }
+//     const len = q.length
+//     let i = 0
+//     while(i < len){
+//       let [node,x] = q.shift()
+//       node.children.map(child => q.push([child,x+1]))
+//       i++
+//     }
+//   }
+//   return Object.values(map).map(val => val)
+// }
