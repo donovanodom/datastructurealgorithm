de1c09a605680c28776105675f1050a44da2a203
donovanodom <dodomdev@gmail.com>
2024-04-01 13:24:33 -0700

    Time: 67 ms (75.93%), Space: 51.7 MB (91.93%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -1,22 +1,24 @@
 const numIslands = (grid) => {
-  let row = grid[0].length, col = grid.length, islands = 0
-  if(row * col === 1) return grid[0][0]
+  let row = grid[0].length, col = grid.length
+  if(row * col == 1) return Number(grid[0][0])
+  let island = 0
   for(let i = 0; i < col; i++){
     for(let j = 0; j < row; j++){
-      if(grid[i][j] === '1'){
+      if(grid[i][j] == '1'){
+        island++
         sinkIsland(grid,j,i)
-        islands++
       }
     }
   }
-  return islands
+  return island
 };
 
 const sinkIsland = (grid,x,y) => {
-  if(x < 0 || y < 0 || x > grid[0].length - 1 || y > grid.length - 1 || grid[y][x] != '1') return
-  grid[y][x] = '0'
-  sinkIsland(grid,x+1,y)
-  sinkIsland(grid,x-1,y)
-  sinkIsland(grid,x,y+1)
-  sinkIsland(grid,x,y-1)
+  if(x >= 0 && y >= 0 && x < grid[0].length && y< grid.length && grid[y][x] == '1'){
+    grid[y][x] = '0'
+    sinkIsland(grid,x+1,y)
+    sinkIsland(grid,x-1,y)
+    sinkIsland(grid,x,y+1)
+    sinkIsland(grid,x,y-1)
+  }
 }
