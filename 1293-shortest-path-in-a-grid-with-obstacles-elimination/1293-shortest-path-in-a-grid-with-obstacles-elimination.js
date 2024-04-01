962391ee49afa2328d29f9ec7f5289d974e66ab1
donovanodom <dodomdev@gmail.com>
2024-04-01 13:17:13 -0700

    Time: 321 ms (59.84%), Space: 72.2 MB (39.34%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.js
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.js
@@ -1,19 +1,18 @@
 const shortestPath = (grid, k) => {
-  const row = grid[0].length, col = grid.length
-  if(row * col == 1) return 0
+  let row = grid[0].length, col = grid.length
+  if(row * col === 1) return 0
   let q = [[0,0,k]], nextQ = [], visited = new Set(`0:0:${k}`), moves = 0
-  const directions = [[1,0],[-1,0],[0,1],[0,-1]]
+  const dirs = [[0,1],[0,-1],[1,0],[-1,0]]
   while(q.length){
-    let [x,y,e] = q.shift()
-    if(x === row - 1 && y == col - 1) return moves
-    for(const [dirX,dirY] of directions){
-      let nextX = x + dirX, nextY = y + dirY
-      if(nextX < 0 || nextY < 0) continue
-      if(nextX > row - 1 || nextY > col - 1) continue
+    const [x,y,e] = q.shift()
+    if(x == row - 1 && y == col - 1) return moves
+    for(const [dirX,dirY] of dirs){
+      let nextX = dirX + x, nextY = dirY + y
+      if(nextX < 0 || nextY < 0 || nextX >= row || nextY >= col) continue
       let obstacle = grid[nextY][nextX]
       if(obstacle && e < 1) continue
       let nextE = e - obstacle
-      let nextP = [nextX,nextY,nextE], nextV = `${nextX}:${nextY}:${nextP}`
+      let nextP = [nextX,nextY,nextE], nextV = `${nextX}:${nextY}:${nextE}`
       if(!visited.has(nextV)){
         visited.add(nextV)
         nextQ.push(nextP)
