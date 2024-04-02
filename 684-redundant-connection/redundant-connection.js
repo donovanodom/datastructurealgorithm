664d4c15213745134b9b808e69f7eb19840e6886
donovanodom <dodomdev@gmail.com>
2024-04-01 21:07:37 -0700

    Time: 56 ms (89.75%) | Memory: 51.5 MB (89.07%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.js
@@ -0,0 +1,20 @@
+const findRedundantConnection = function(edges) {
+    const parents = Array(edges.length).fill().map((_,i) => i)
+    for(const [a,b] of edges){
+        if(union(parents,a,b)) return [a,b]
+    }
+}
+
+const union = (parents,x,y) => {
+    let repx = find(parents,x), repy = find(parents,y)
+    if(repx == repy) return true
+    parents[repy] = repx
+}
+
+const find = (parents,node) => {
+    if(parents[node] == node){
+        return node
+    }else{
+        return find(parents,parents[node])  
+    }
+}
