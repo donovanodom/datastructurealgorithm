9ed741bc9f1cb5368c96f6044d88ff4993d52487
donovanodom <dodomdev@gmail.com>
2024-04-02 20:21:33 -0700

    Time: 261 ms (5.03%), Space: 60.1 MB (5.03%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.js
+++ 0684-redundant-connection/0684-redundant-connection.js
@@ -1,20 +1,21 @@
 const findRedundantConnection = function(edges) {
-    const parents = Array(edges.length).fill().map((_,i) => i)
-    for(const [a,b] of edges){
-        if(union(parents,a,b)) return [a,b]
-    }
+  const parents = Array(edges.length + 1).fill().map((_,i) => i)
+  for(const [a,b] of edges){
+    console.log(parents)
+    if(union(parents,a,b)) return [a,b]
+  }
 }
 
 const union = (parents,x,y) => {
-    let repx = find(parents,x), repy = find(parents,y)
-    if(repx == repy) return true
-    parents[repy] = repx
+  let repx = find(parents,x), repy = find(parents,y)
+  if(repx == repy) return true
+  parents[repy] = repx
 }
 
 const find = (parents,node) => {
-    if(parents[node] == node){
-        return node
-    }else{
-        return find(parents,parents[node])  
-    }
+  if(parents[node] == node){
+    return node
+  }else{
+    return find(parents,parents[node])  
+  }
 }
