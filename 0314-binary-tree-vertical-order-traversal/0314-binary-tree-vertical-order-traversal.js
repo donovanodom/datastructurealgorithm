22badc7ef73b6eefa2fedd2d5f38936b739087e6
donovanodom <dodomdev@gmail.com>
2024-04-28 11:12:56 -0700

    Time: 60 ms (66.61%), Space: 52 MB (29.65%) - LeetHub

---
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.js
@@ -0,0 +1,16 @@
+const verticalOrder = function(root) {
+  let map = traverse(root)
+  return Object.values(map).map(val => {
+    val.sort((a,b) => a[0] - b[0])
+    return val.map(val => val[1])
+  })
+};
+
+const traverse = (node, row = 0, col = 100, map = {}) => {
+  if(!node) return []
+  if(!map[col]) map[col] = []
+  map[col].push([row, node.val])
+  if(node.left) traverse(node.left, row + 1, col - 1, map)
+  if(node.right) traverse(node.right, row + 1, col + 1, map)
+  return map
+}
