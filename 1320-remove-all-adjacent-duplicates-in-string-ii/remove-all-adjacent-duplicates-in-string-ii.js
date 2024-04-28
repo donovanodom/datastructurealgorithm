8d283db6c4ca52746b669d18039d666b8df15564
donovanodom <dodomdev@gmail.com>
2024-04-28 10:52:26 -0700

    Time: 85 ms (33.12%) | Memory: 58 MB (71.06%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.js
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.js
@@ -4,17 +4,17 @@ const removeDuplicates = (s, k) => {
   }
   const stack = []
   for(const char of s){
-    if(stack.length && char == stack[stack.length - 1][0]){
+    if(stack.length && stack[stack.length - 1][0] == char){
       stack[stack.length - 1][1]++
       if(stack[stack.length - 1][1] == k){
-         stack.pop()       
+        stack.pop()
       }
     }else{
-      stack.push([char,1])
+      stack.push([char, 1])
     }
   }
   let res = ''
-  for(const [char,count] of stack){
+  for(const [char, count] of stack){
     res += char.repeat(count)
   }
   return res
