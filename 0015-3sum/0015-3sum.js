9caad2b436d68f8d8a896f9c41b1ee186631787e
donovanodom <dodomdev@gmail.com>
2024-04-29 10:45:34 -0700

    Time: 160 ms (62.17%), Space: 65.6 MB (47.36%) - LeetHub

---
+++ 0015-3sum/0015-3sum.js
@@ -0,0 +1,23 @@
+const threeSum = function(nums) {
+  nums.sort((a,b) => a - b)
+  const res = []
+  for(let i = 0; i < nums.length; i++){
+    let low = i + 1, high = nums.length - 1, sum = 0
+    while(low < high){
+      sum = nums[i] + nums[low] + nums[high]
+      if(sum === 0){
+        res.push([nums[i],nums[low],nums[high]])
+        while(nums[low] == nums[low + 1]) low++
+        while(nums[high] == nums[high - 1]) high--
+        low++
+        high--
+      }else if(sum < 0){
+        low++
+      }else{
+        high--
+      }
+    }
+    while(nums[i] == nums[i + 1]) i++
+  }
+  return res
+}
