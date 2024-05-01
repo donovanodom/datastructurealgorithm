db110fb2134511501bd5f47e4044c7ee99fbac2f
donovanodom <dodomdev@gmail.com>
2024-05-01 14:22:21 -0700

    Time: 59 ms (100.00%) | Memory: 52.9 MB (94.67%) - LeetSync

--- 694-number-of-distinct-islands/number-of-distinct-islands.js
+++ 694-number-of-distinct-islands/number-of-distinct-islands.js
@@ -1,17 +1,21 @@
 const numDistinctIslands = (grid) => {
-  const row = grid[0].length, col = grid.length, set = new Set()
+  const row = grid[0].length, col = grid.length, res = new Set()
   for(let i = 0; i < col; i++){
     for(let j = 0; j < row; j++){
-      if(grid[i][j]) set.add(sinkIsland(grid, j, i, 'S'))
+      if(grid[i][j]) res.add(sinkIsland(grid, j, i, 'S'))
     }
   }
-  return set.size
+  return res.size
 };
 
 const sinkIsland = (grid, x, y, path) => {
-  if(x < 0 || y < 0 || x >= grid[0].length || y >= grid.length || grid[y][x] == 0){
-    return ''
-  }
+  const row = grid[0].length, col = grid.length
+  if(x >= row || x < 0 || y >= col || y < 0 || grid[y][x] == 0) return ''
   grid[y][x] = 0
-  return path + sinkIsland(grid, x + 1, y, 'R') + sinkIsland(grid, x - 1, y, 'L') + sinkIsland(grid, x, y + 1, 'U') + sinkIsland(grid, x, y - 1, 'D') + 'S'
-} 
+  return path + 
+    sinkIsland(grid, x + 1, y, 'R') + 
+    sinkIsland(grid, x - 1, y, 'L') +
+    sinkIsland(grid, x, y - 1, 'U') +
+    sinkIsland(grid, x, y + 1, 'D') +
+    'S'
+};
