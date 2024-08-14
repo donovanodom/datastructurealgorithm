70adf5710e1c71c444f5812042cca9882172cbb8
donovanodom <dodomdev@gmail.com>
2024-08-14 15:41:48 -0700

    Time: 63 ms (12.40%), Space: 50.9 MB (6.53%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -1,5 +1,19 @@
 const merge = (nums1, m, nums2, n) => {
-  nums1.splice(nums1.length - n, n)
-  nums1.push(...nums2)
-  return nums1.sort((a,b) => a - b)
+  const map = {}, len = nums1.length
+  for(let i = 0; i < len - m; i++){
+    nums1.pop()
+  }
+  
+  for(const num of nums2){
+    let push = num + 1000000000
+    if(!map[push]) map[push] = 0
+    map[push]++
+  }
+  const keys = Object.keys(map).map((key) => key - 1000000000)
+  let i = 0
+  for(const key of keys){
+    while(key > nums1[i]) i++
+    let arr = Array(map[key + 1000000000]).fill(Number(key)) 
+    nums1.splice(i, 0, ...arr)
+  }
 }
