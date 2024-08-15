f99f419f498fb2dc83d2b84d9c0a779af2f96da1
donovanodom <dodomdev@gmail.com>
2024-08-15 13:57:28 -0700

    Time: 158 ms (59.19%) | Memory: 65.1 MB (64.00%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -1,23 +1,23 @@
 const threeSum = function(nums) {
   nums.sort((a,b) => a - b)
   const res = []
-  for(let i = 0; i < nums.length; i++){
-    let low = i + 1, high = nums.length - 1, sum = 0
+  for(let start = 0; start < nums.length; start++){
+    let low = start + 1, high = nums.length - 1
     while(low < high){
-      sum = nums[i] + nums[low] + nums[high]
-      if(sum === 0){
-        res.push([nums[i],nums[low],nums[high]])
+      let sum = nums[start] + nums[low] + nums[high]
+      if(sum == 0){
+        res.push([nums[start], nums[low], nums[high]])
         while(nums[low] == nums[low + 1]) low++
         while(nums[high] == nums[high - 1]) high--
         low++
         high--
-      }else if(sum < 0){
+      } else if(sum < 0) {
         low++
-      }else{
+      } else {
         high--
       }
     }
-    while(nums[i] == nums[i + 1]) i++
+    while(nums[start] == nums[start + 1]) start++
   }
   return res
 }
