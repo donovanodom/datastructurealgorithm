2f08db5c039e6ace155d169f143ce9a10c427412
donovanodom <dodomdev@gmail.com>
2024-09-05 14:47:13 -0700

    Time: 802 ms (11.63%), Space: 89.9 MB (39.53%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.js
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.js
@@ -2,16 +2,17 @@ const minimumObstacles = function (grid) {
   const row = grid.length, col = grid[0].length
   const dp = new Array(row).fill().map(() => new Array(col).fill(Infinity))
   dp[0][0] = 0
-  const q = [[0,0]], dirs = [[1,0],[-1,0],[0,1],[0,-1]]
-  while(q.length){
-    const [x,y] = q.shift(), tax = dp[x][y]
+  const heap = new MinPriorityQueue(), dirs = [[1,0],[-1,0],[0,1],[0,-1]]
+  heap.enqueue([0,0],0)
+  while(heap.size()){
+    const [x,y] = heap.dequeue().element, tax = dp[x][y]
+    if(x == row - 1 && y == col - 1) return tax
     for(const [dirX, dirY] of dirs){
       const nextX = x + dirX, nextY = y + dirY
       if(nextX >= 0 && nextY >= 0 && nextX < row && nextY < col && tax + grid[nextX][nextY] < dp[nextX][nextY]){
         dp[nextX][nextY] = tax + grid[nextX][nextY]
-        q.push([nextX,nextY])
+        heap.enqueue([nextX,nextY],tax)
       }
     }
   }
-  return dp[row - 1][col -1]
 };
