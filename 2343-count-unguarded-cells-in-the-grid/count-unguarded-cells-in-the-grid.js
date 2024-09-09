e712d576ae3283a561f70ee53464720dbf2fc7c1
donovanodom <dodomdev@gmail.com>
2024-09-08 21:33:31 -0700

    Time: 518 ms (39.29%) | Memory: 89.4 MB (35.71%) - LeetSync

---
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.js
@@ -0,0 +1,42 @@
+const countUnguarded = function(m, n, guards, walls) {
+  const mat = Array(m).fill().map(() => Array(n).fill(0))
+  const row = mat.length, col = mat[0].length
+  let total = row * col
+  for(const [x,y] of walls){
+    mat[x][y] = 'W'
+    total--
+  }
+  const traverse = (initx, inity) => {
+  const q = [[initx,inity,'']], dirs = {'u':[-1,0],'d':[1,0],'l':[0,-1],'r':[0,1]}
+  while(q.length){
+    const [x,y,dir] = q.shift()
+    if(!dir){
+      Object.keys(dirs).map((key) => {
+        const [dirX, dirY] = dirs[key], nextX = x + dirX, nextY = y + dirY
+        if(nextX >= 0 && nextY >= 0 && nextX < row && nextY < col && mat[nextX][nextY] != 'W' && mat[nextX][nextY] != 'G'){
+          if(mat[nextX][nextY] == 0) total--
+          mat[nextX][nextY] = 1
+          q.push([nextX,nextY,key])
+        }
+      })
+    }else{
+      const [dirX, dirY] = dirs[dir], nextX = x + dirX, nextY = y + dirY
+      if(nextX >= 0 && nextY >= 0 && nextX < row && nextY < col && mat[nextX][nextY] != 'W' && mat[nextX][nextY] != 'G'){
+        if(mat[nextX][nextY] == 0) total--
+        mat[nextX][nextY] = 1
+        q.push([nextX,nextY,dir])
+      }
+    }
+  }
+}
+  for(const [x,y] of guards){
+    mat[x][y] = 'G'
+    total--
+  }
+  for(const [x,y] of guards){
+    traverse(x, y, mat, row, col, total)
+  }
+
+  return total
+}
+
