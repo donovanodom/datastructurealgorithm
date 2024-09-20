4b276ceb31759b1576dcfdc00a0fe2278008a4fb
donovanodom <dodomdev@gmail.com>
2024-09-19 21:28:48 -0700

    Time: 394 ms (85.71%), Space: 80.2 MB (78.57%) - LeetHub

--- 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.js
+++ 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.js
@@ -1,23 +1,52 @@
 const minimumTime = function(grid) {
+  
+  // store row and column lengths
   const row = grid.length, col = grid[0].length
+  
+  // return -1 if top-left cell is gridlocked
   if(grid.length > 1 && grid[0].length > 1 && grid[1][0] > 1 && grid[0][1] > 1) return -1
+  
+  // initialize dynamic programming matrix with Infinity values and set top-left cell to 0
   const dp = Array(grid.length).fill().map(() => Array(grid[0].length).fill(Infinity))
   dp[0][0] = 0
+  
+  // initialize a minimum priority heap and an array of direction coordinates
+  // add the coordinates, time and priority for start point of top-left cell
   const heap =  new MinPriorityQueue(), dirs = [[1,0],[-1,0],[0,1],[0,-1]]
   heap.enqueue([0,0,0],0)
+  
+  // iterate through heap to determine next cell with lowest time
   while(heap.size()){
-    const [x,y,t] = heap.dequeue().element
-    if(x == row - 1 && y == col - 1) return t
+    const [x,y,time] = heap.dequeue().element
+    
+    // if we have arrived at the end of the matrix return time value
+    if(x == row - 1 && y == col - 1) return time
+    
+    // iterate directions and create values for possible next coordinates in our matrix
     for(const [dirX,dirY] of dirs){
       const nextX = x + dirX, nextY = y + dirY
+      
+      // check if next coordinates are out of bounds
       if(nextX >= 0 && nextY >= 0 && nextX < row && nextY < col){
-        const nextT = grid[nextX][nextY] > t && (grid[nextX][nextY] - t) % 2 == 0 ? grid[nextX][nextY] + 1 : Math.max(grid[nextX][nextY], t + 1)
-        if(nextT < dp[nextX][nextY]){
-          dp[nextX][nextY] = nextT
-          heap.enqueue([nextX,nextY,nextT],nextT)
+        
+        // since you can bounce back and fourth between prior cells to 
+        // reach the next value, set next time to one greater than next 
+        // cell value if the difference between the two values is even, 
+        // if the difference is not divisible by 2, set next time to the 
+        // max value of the next cell value and current time + 1
+        const nextTime = grid[nextX][nextY] > time && (grid[nextX][nextY] - time) % 2 == 0 ? grid[nextX][nextY] + 1 : Math.max(grid[nextX][nextY], time + 1)
+        
+        // if our next time is less than any values we've encountered at 
+        // this  location, set the dp coordinates to next time and add the 
+        // next coordinates and next time to the minimum priorty queue
+        if(nextTime < dp[nextX][nextY]){
+          dp[nextX][nextY] = nextTime
+          heap.enqueue([nextX,nextY,nextTime],nextTime)
         }
       }
     }
   }
+  
+  // return the minimum time it took to reach the last cell
   return dp[row-1][col-1]
 }
