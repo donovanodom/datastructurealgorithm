c79eae275819611f0c7a1aafc3bd6b927fef39e0
donovanodom <dodomdev@gmail.com>
2024-09-28 14:23:14 -0700

    Time: 83 ms (41.72%), Space: 58.6 MB (38.35%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.js
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.js
@@ -1,34 +1,51 @@
 const minRemoveToMakeValid = function(s) {
-    const open = [], closed = []
-    for(i = 0; i < s.length; i++){
-        let char = open[open.length - 1]
-        if(s[i] != '(' && s[i] != ')') continue
-        if(s[i] == '('){
-            open.push(s[i])
-        }else if(s[i] = ')' && char == '('){
-            open.pop()
-        }else{
-            closed.push(s[i])
-        }
-        
+  
+  // intialize stacks for open and closed parentheses
+  const open = [], closed = []
+  
+  // iterate through string and store open and closed
+  // parentheses, whenever you encounter a closed
+  // parentheses with an open one existing in the open
+  // stack, remove the last element from the stack and
+  // do not store the closed parenteses
+  for(i = 0; i < s.length; i++){
+    if(s[i] == '('){
+      open.push(s[i])
+    }else if(s[i] == ')' && open.length){
+      open.pop()
+    }else if(s[i] == ')'){
+      closed.push(s[i])
     }
-    if(!open.length && !closed.length) return s
-    if(open.length + closed.length == s.length) return ""
-    let start = 0, end = s.length - 1
-    while(open.length || closed.length){
-        if(s[start] != ')' && closed.length){
-            start++
-        }else if(closed.length){
-            closed.pop()
-            s = s.slice(0, start) + s.slice(start + 1)
-        }
-        if(s[end] != '(' && open.length){
-            end--
-        }else if(open.length){
-            open.pop()    
-            s = s.slice(0, end) + s.slice(end + 1)
-            end--
-        }
-    }   
-    return s
+  }
+  
+  // if open and closed stacks are empty there are
+  // no parentheses to remove so return s
+  if(!open.length && !closed.length) return s
+  
+  // if the string length equals all stored parentheses you
+  // must remove all ie "))((" and return an empty string
+  if(open.length + closed.length == s.length) return ""
+  
+  // as long as the stacks have values, iterate through
+  // string from left to remove the first closed
+  // parentheses you encounter and from the right to 
+  // remove the first open parentheses you encounter
+  let start = 0, end = s.length - 1
+  while(closed.length){
+    if(s[start] == ')' && closed.length){
+      closed.pop()
+      s = s.slice(0, start) + s.slice(start + 1)
+    }else{
+      start++
+    }
+  }
+  while(open.length){
+    if(s[end] == '(' && open.length){
+      open.pop()    
+      s = s.slice(0, end) + s.slice(end + 1)
+    }else{
+      end--
+    }
+  }   
+  return s
 }
