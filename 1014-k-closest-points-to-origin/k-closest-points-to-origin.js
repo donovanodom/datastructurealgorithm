9840ab2d22cc36f4aa51faa9d52ff512ad5b1815
donovanodom <dodomdev@gmail.com>
2024-09-30 18:44:52 -0700

    Time: 268 ms (18.16%) | Memory: 73.2 MB (5.96%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.js
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.js
@@ -1,9 +1,13 @@
 const kClosest = function(points, k) {
-  let q = []
+  let q = new MinPriorityQueue()
   for(const [x,y] of points){
     const distance = Math.sqrt(Math.pow(x,2)+Math.pow(y,2))
-    q.push([[x,y],distance])
+    q.enqueue([x,y],distance)
   }
-  q.sort((a,b) => a[1] - b[1])
-  return q.map((el) => el[0]).slice(0,k)
+  const ans = []
+  while(k){
+    ans.push(q.dequeue().element)
+    k--
+  }
+  return ans
 }
