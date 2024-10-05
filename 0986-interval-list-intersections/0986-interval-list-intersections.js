084827918211b091e43ee0248e5dd1846bdf9338
donovanodom <dodomdev@gmail.com>
2024-10-04 18:18:10 -0700

    Time: 173 ms (5.17%), Space: 61.2 MB (5.17%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.js
+++ 0986-interval-list-intersections/0986-interval-list-intersections.js
@@ -1,5 +1,9 @@
 const intervalIntersection = function(firstList, secondList) {
   const result = []
+  
+  // iterate first and second list then check to see if an
+  // intersection exist. Call the helper function to calculate
+  // the intersection and push return value to result
   for(const [a1, b1] of firstList){
     for(const [a2, b2] of secondList){
       if((a1 > b2) || (b1 < a2)) continue
@@ -13,6 +17,8 @@ const intervalIntersection = function(firstList, secondList) {
 
 const checkIntersection = (first, second) => {
   const [a1, b1] = first, [a2, b2] = second
+  
+  // check if there is any overlap in the intervals
   if(
     (a1 <= a2 && a2 <= b1) || 
     (a2 <= a1 && a1 <= b2) || 
@@ -24,5 +30,8 @@ const checkIntersection = (first, second) => {
 
 const calcIntersection = (first, second) => {
   const [a1, b1] = first, [a2, b2] = second
+  
+  // return the minimum start and maximum end of
+  // the combination of the two intervals
   return [Math.max(a1,a2), Math.min(b1,b2)]
 }
