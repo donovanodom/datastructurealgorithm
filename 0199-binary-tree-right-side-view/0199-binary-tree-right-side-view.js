c648d7c5c03bb7d4384b10b2b6a605d2112d4277
donovanodom <dodomdev@gmail.com>
2024-10-05 14:19:25 -0700

    Time: 59 ms (56.68%), Space: 51.4 MB (39.04%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -1,5 +1,13 @@
 const rightSideView = (root) => {
-  const map = {}, ans = []
+  
+  // initialize a map object to hold an array of values of
+  // each node at a given level in the tree and a result 
+  // array to hold the last value of each of those arrays
+  const map = {}, result = []
+  
+  // recursively traverse through the tree, prioritizing the 
+  // left side first to ensure the last value to the right
+  // on each level is the last value pushed to the level array
   const traverse = (root, level = 1) => {
     if(!root) return 
     if(!map[level]) map[level] = []
@@ -8,8 +16,10 @@ const rightSideView = (root) => {
     if(root.right) traverse(root.right, level + 1)
   }
   traverse(root)
+  
+  // remove the last value from each level array and add to result
   Object.values(map).forEach((val) => {
-    ans.push(val.pop())
+    result.push(val.pop())
   })
-  return ans
+  return result
 }
