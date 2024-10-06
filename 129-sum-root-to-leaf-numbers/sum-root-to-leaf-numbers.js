5c16e0967b27eb6aaa8b77f5fbd8facddf6f534f
donovanodom <dodomdev@gmail.com>
2024-10-06 15:27:19 -0700

    Time: 57 ms (48.16%) | Memory: 50.6 MB (31.43%) - LeetSync

---
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.js
@@ -0,0 +1,9 @@
+const sumNumbers = function(root, str = '', sum = 0) {
+  if(!root) return 0
+  str += root.val
+  if(!root.left && !root.right){
+    sum += Number(str)
+    return sum
+  }
+  return sumNumbers(root.left, str, sum) + sumNumbers(root.right, str, sum)
+}
