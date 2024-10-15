b6a7aeb8611604f208c3872f44e5570603b276c8
donovanodom <dodomdev@gmail.com>
2024-10-15 14:38:13 -0700

    Time: 58 ms (79.38%) | Memory: 54 MB (38.14%) - LeetSync

---
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.js
@@ -0,0 +1,14 @@
+const averageOfSubtree = function(root) {
+  let count = 0
+  const traverse = (node) => {
+    if(!node) return [0, 0]
+    let left = traverse(node.left)
+    let right = traverse(node.right)
+    let sum = node.val + left[0] + right[0]
+    let total = left[1] + right[1] + 1
+    if(node.val == Math.floor(sum/total)) count++
+    return[sum, total]
+  }
+  traverse(root)
+  return count
+}
