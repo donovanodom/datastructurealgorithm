81bf11da066aae9b7cab328c9f5e2b3ba3383e5d
donovanodom <dodomdev@gmail.com>
2024-10-16 12:27:53 -0700

    Time: 100 ms (5.99%) | Memory: 55.6 MB (5.39%) - LeetSync

---
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.js
@@ -0,0 +1,36 @@
+const isCompleteTree = (root) => {
+  const levels = []
+  levels[0] = []
+  const traverse = (node, level = 0) => {
+    if(!node){
+      if(!levels[level]) levels[level] = []
+      levels[level].push(null)
+      return
+    }
+    traverse(node.left, level + 1)
+    traverse(node.right, level + 1)
+    if(!levels[level]) levels[level] = []
+    levels[level].push(node.val)
+  }
+  traverse(root)
+  console.log(levels)
+  return validLevels(levels)
+}
+
+const validLevels = (levels) => {
+  let i = 0, end = false
+  while(i < levels.length){
+    let j = 0
+    while(j < levels[i].length){
+      if(levels[i][j] == null){
+        end = true
+        if(levels[i][j + 1]) return false
+      }else if(end){
+        return false
+      }
+      j++
+    }
+    i++
+  }
+  return true
+}
