fc2d706cbbf683ee449039c0e4e74203bf03f689
donovanodom <dodomdev@gmail.com>
2024-10-18 13:25:29 -0700

    Time: 156 ms (17.98%), Space: 68.6 MB (55.06%) - LeetHub

--- 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.js
+++ 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.js
@@ -1,17 +1,39 @@
 const lowestCommonAncestor = (root, p, q) => {
-  const findNodes = (node, target) => {
+  
+  // helper function to determine if node exists in tree
+  const findNode = (node, target) => {
+    
+    // return false if a non-existent node is reached
+    // and return true if the target node is reached
     if(!node) return false
     if(node == target) return true
-    const left = findNodes(node.left, target)
-    const right = findNodes(node.right, target)
+    
+    // traverse through left and right branches of the tree
+    const left = findNode(node.left, target)
+    const right = findNode(node.right, target)
+    
+    // return true if either left or right reaches target
     return left || right
   }
+  
+  // helper function for finding least common ancestor
   const findLCA = (node, p, q) => {
+    
+    // return node whether we reach either node or null node
     if(!node || node == p || node == q) return node
+    
+    // traverse through left and right branches of the tree
     const left = findLCA(node.left, p, q)
     const right = findLCA(node.right, p, q)
+    
+    // return node if both left and right reaches targets 
+    // or either node that can be reached making that 
+    // node the least common ancestor and the other a descendant
     if(left && right) return node
     return left ?? right
   }
-  return (findNodes(root,p) && findNodes(root,q)) && findLCA(root, p, q)
+    
+  // if either node does not exist in tree, return false, otherwise
+  // return the node from findLCA helper function
+  return (findNode(root,p) && findNode(root,q)) && findLCA(root, p, q)
 }
