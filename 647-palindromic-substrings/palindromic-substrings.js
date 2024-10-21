62d68fa8cf5ac9c6fb95266e5c63acae759d89ac
donovanodom <dodomdev@gmail.com>
2024-10-21 10:26:07 -0700

    Time: 11 ms (99.67%) | Memory: 49.7 MB (82.48%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.js
+++ 647-palindromic-substrings/palindromic-substrings.js
@@ -1,4 +1,11 @@
 const countSubstrings = function(s) {
+  
+  // initialize count to zero and iterate through
+  // s for each char in s run the helper function
+  // to count out from the center to find palidromes
+  // since some palidromes count have an odd number
+  // making the middle character inconsequential,
+  // start at both i & i and i & i + 1 for even lengths
   let count = 0, i = 0
   while(i < s.length){
     count += fromCenter(s, i, i)
