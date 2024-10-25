379d9ebfd29dfb844fa8be10558a8350874cd82d
donovanodom <dodomdev@gmail.com>
2024-10-25 09:46:04 -0700

    Time: 1 ms (98.45%), Space: 51.8 MB (41.50%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.js
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.js
@@ -1,45 +1,12 @@
-const verticalOrder = function(root) {
-  
-  // assign map to traverse function
-  let map = traverse(root)
-  
-  // iterate through map object and return each col values 
-  // from left to right after sorting each array by their row
-  return Object.values(map).map((values) => {
-    
-    // sorting by row ensures vertical order since the top most
-    // value should have the lowest row value
-    values.sort((a,b) => a[0] - b[0])
-    
-    // return only the node value and now the array
-    return values.map((val) => val[1])
-  })
-}
-
-// traverse function will traverse the tree and store the values
-// and row of the a node for a given column, initialize the row 
-// to 0 for the starting node and col to 100 given that the 
-// constraints are -100 <= Node.val <= 100, we should never 
-// reach a column less than zero
-const traverse = (node, row = 0, col = 100, map = {}) => {
-  
-  // if no node given in first call, return an empty array
-  if(!node) return []
-  
-  // initialize key of col to an empty array
-  if(!map[col]) map[col] = []
-  
-  // store row and value pair to col
-  map[col].push([row,node.val])
-  
-  // if node.left exists, recursively call with function with
-  // row + 1 (going down tree) and col - 1 (going left tree)
-  if(node.left) traverse(node.left, row + 1, col - 1, map)
-  
-  // if node.right exists, recursively call with function with
-  // row + 1 (going down tree) and col + 1 (going right tree)
-  if(node.right) traverse(node.right, row + 1, col + 1, map)
-  
-  // return map object once traversal is completed
-  return map
+const verticalOrder = (root) => {
+  const map = {}
+  const traverse = (node, col = 100, row = 1) => {
+    if(!node) return
+    if(!map[col]) map[col] = []
+    map[col].push([row, node.val])
+    traverse(node.left, col - 1, row + 1)
+    traverse(node.right, col + 1, row + 1)
+  }
+  traverse(root)
+  return Object.values(map).map((val) => val.sort((a,b) => a[0] - b[0]).map((val) => val[1]))
 }
