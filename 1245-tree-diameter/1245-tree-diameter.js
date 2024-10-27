4ed1d2103c174b03c82fffbd8b1e7a5a213e821f
donovanodom <dodomdev@gmail.com>
2024-10-26 21:15:26 -0700

    Time: 47 ms (25.00%), Space: 61.8 MB (25.00%) - LeetHub

--- 1245-tree-diameter/1245-tree-diameter.js
+++ 1245-tree-diameter/1245-tree-diameter.js
@@ -1,17 +1,27 @@
 const treeDiameter = (edges) => {
+  
+  // if empty array return 0 as max
   if(!edges.length) return 0
-  const adj = {}, maxPaths = {}
+  
+  // create an adjacency list for edges between nodes
+  const adj = {}
   for(const [x,y] of edges){
     if(!adj[x]) adj[x] = []
     adj[x].push(y)
     if(!adj[y]) adj[y] = []
     adj[y].push(x)
   }
+  
+  // define furthest as the furthest node from any 
+  // node and define count as the distnace of the 
+  // furthest node from that node and return count
   const furthest = followFurthest(0,adj)[0]
   const count = followFurthest(furthest,adj)[1]
   return count
 }
 
+// helper function that returns the furthest node
+// and distance from a given node and adjacency list
 const followFurthest = (node, list) => {
   const seen = new Set(), q = [[node,0]]
   seen.add(node)
@@ -23,6 +33,9 @@ const followFurthest = (node, list) => {
         q.push([next,len+1])
       }
     }
+    
+    // if queue is empty the furthest possible node
+    // must be the current node in our while loop
     if(!q.length) return [cur,len]
   }
 }
