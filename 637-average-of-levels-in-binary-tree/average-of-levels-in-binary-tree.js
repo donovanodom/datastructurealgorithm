a87acc7a9ac31ef8a14191538f288ac85b1754d5
donovanodom <dodomdev@gmail.com>
2024-11-11 11:55:55 -0800

    Time: 3 ms (72.05%) | Memory: 55.4 MB (18.51%) - LeetSync

---
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.js
@@ -0,0 +1,15 @@
+const averageOfLevels = function(root) {
+  const levels = {}
+  const traverse = (node, level = 1) => {
+    if(!node) return
+    if(!levels[level]) levels[level] = [0,0]
+    levels[level][0] += node.val
+    levels[level][1]++
+    traverse(node.left, level + 1)
+    traverse(node.right, level + 1)
+  }
+  traverse(root)
+  return Object.values(levels).map(([sum,count]) => {
+    return sum / count
+  })
+}
