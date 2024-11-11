641310f24a4aa0201f4c7af5af29f1a363e73c07
donovanodom <dodomdev@gmail.com>
2024-11-11 13:21:28 -0800

    Time: 25 ms (88.89%) | Memory: 60.1 MB (40.00%) - LeetSync

--- 2669-find-the-substring-with-maximum-cost/find-the-substring-with-maximum-cost.js
+++ 2669-find-the-substring-with-maximum-cost/find-the-substring-with-maximum-cost.js
@@ -1,5 +1,11 @@
 const maximumCostSubstring = (s, chars, vals) => {
+  
+  //initialize arr to hold values for each character
   const arr = []
+  
+  // traverse characters in string and push their value
+  // to arr if in vals array, else get their value using
+  // the char code for the character
   for(const char of s){
     const idx = chars.indexOf(char)
     if(idx >= 0){
@@ -8,6 +14,8 @@ const maximumCostSubstring = (s, chars, vals) => {
       arr.push(char.charCodeAt(0) - 96)
     }
   }
+  
+  // use Kadane's algorithms to find maximum sub value
   let curMax = 0, finalMax = 0
   for(const cost of arr){
     curMax = Math.max(0, curMax + cost)
