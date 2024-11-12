810c5f9bdd2b4c3a47406f5890937969df686f59
donovanodom <dodomdev@gmail.com>
2024-11-11 16:06:23 -0800

    Time: 15 ms (100.00%), Space: 59.6 MB (100.00%) - LeetHub

--- 1946-largest-number-after-mutating-substring/1946-largest-number-after-mutating-substring.js
+++ 1946-largest-number-after-mutating-substring/1946-largest-number-after-mutating-substring.js
@@ -1,15 +1,21 @@
 const maximumNumber = function(num, change) {
-  let hasChanges = false, i = 0
+  let hasChanges = false, i = 0, start, end, diff = ''
   while(i < num.length){
     while(i < num.length && change[Number(num[i])] >= num[i]){      
       if(change[Number(num[i])] > num[i]){
-        num = num.slice(0,i) + change[Number(num[i])] + num.slice(i + 1)
+        diff += change[Number(num[i])]
+        if(!hasChanges) start = i
         hasChanges = true
+      }else if(hasChanges){
+        diff += num[i]
       } 
       i++
     }
-    if(hasChanges) break
+    if(hasChanges){
+      end = i
+      break
+    } 
     i++
   }
-  return num
+  return hasChanges ? num.slice(0,start) + diff + num.slice(end) : num
 }
