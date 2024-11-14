f12aa8ae187e5675926a17751598cdbbf2792047
donovanodom <dodomdev@gmail.com>
2024-11-14 13:10:10 -0800

    Time: 0 ms (100.00%) | Memory: 51.3 MB (70.69%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.js
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.js
@@ -1,12 +1,12 @@
-const verticalOrder = (root) => {
-  const map = {}
-  const traverse = (node, col = 100, row = 1) => {
-    if(!node) return
+const verticalOrder = function(root) {
+  if(!root) return []
+  const map = {}, q = [[root, 100]]
+  while(q.length){
+    const [node, col] = q.shift()
     if(!map[col]) map[col] = []
-    map[col].push([row, node.val])
-    traverse(node.left, col - 1, row + 1)
-    traverse(node.right, col + 1, row + 1)
+    map[col].push(node.val)
+    if(node.left) q.push([node.left, col - 1])
+    if(node.right) q.push([node.right, col + 1])
   }
-  traverse(root)
-  return Object.values(map).map((val) => val.sort((a,b) => a[0] - b[0]).map((val) => val[1]))
+  return Object.values(map)
 }
