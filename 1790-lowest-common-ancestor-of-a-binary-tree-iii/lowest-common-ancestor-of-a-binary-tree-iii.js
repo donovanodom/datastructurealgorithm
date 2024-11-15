2b4ac978392c56a8e8dcb1aa67047b1e8843ad75
donovanodom <dodomdev@gmail.com>
2024-11-15 10:46:25 -0800

    Time: 72 ms (25.32%) | Memory: 55.2 MB (18.21%) - LeetSync

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.js
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.js
@@ -1,24 +1,12 @@
 const lowestCommonAncestor = function(p, q) {
-  
-  // initialize Set to store values we come across
-  // traversing upwards from p to root and add 
-  // initial value of p node
   const seen = new Set()
-  seen.add(p.val)
-  
-  // add the remainder of values from p to root
   while(p.parent){
-    p = p.parent
     seen.add(p.val)
+    p = p.parent
   }
-  
-  // if our initial q value exists in Set return value
-  if(seen.has(q.val)) return q
-  
-  // traverse upwards from q to root and return
-  // the first value that exists in Set
   while(q.parent){
-    q = q.parent
     if(seen.has(q.val)) return q
+    q = q.parent
   }
+  return q
 }
