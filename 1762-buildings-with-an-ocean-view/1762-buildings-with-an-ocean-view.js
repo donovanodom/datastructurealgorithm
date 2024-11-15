34584f7ef0c9ab81ebf9b6c37a39d2d5405e2786
donovanodom <dodomdev@gmail.com>
2024-11-15 10:56:04 -0800

    Time: 643 ms (13.60%), Space: 68.8 MB (61.26%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.js
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.js
@@ -1,14 +1,10 @@
-/**
- * @param {number[]} heights
- * @return {number[]}
- */
 const findBuildings = function(heights) {
-  let max = 0, ans = []
+  let max = 0, res = []
   for(let i = heights.length - 1; i >= 0; i--){
     if(heights[i] > max){
-      ans.unshift(i)
-      max = Math.max(heights[i],max)
+      res.unshift(i)
+      max = heights[i]
     }
   }
-  return ans
+  return res
 }
