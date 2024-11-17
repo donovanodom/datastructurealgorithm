09810d3646e300f70342b166d3227031f67f9c79
donovanodom <dodomdev@gmail.com>
2024-11-17 11:59:15 -0800

    Time: 1 ms (92.00%) | Memory: 50.2 MB (96.53%) - LeetSync

--- 71-simplify-path/simplify-path.js
+++ 71-simplify-path/simplify-path.js
@@ -1,27 +1,13 @@
 const simplifyPath = (path) => {
-  
-  // initialize a stack and an array of files
-  // split by '/' this will eliminate any
-  // duplicate forward slashes and leave only 
-  // with files and dot sequences
-  const stack = [], files = path.split('/')
-  
-  // iterate through files and if we encounter
-  // double dots and a files in the stack, 
-  // remove the most recent file symbolizing
-  // going back one level in the file tree
-  for(const file of files){
-    if(file == '..'){
-      if(stack.length) stack.pop()
-      
-     // if the file is a valid string and not a 
-     // singulare dot add it to the stack
-    }else if(file && file != '.'){
-      stack.push(file)
+  const stack = [], arr = path.split('/')
+  for(const el of arr){
+    if(el == '..'){
+      stack.pop()
+    }else if(el == '.'){
+      continue
+    }else if(el){
+      stack.push(el)
     }
   }
-  
-  // begin the path with a forward slash and
-  // append the files joined by a forward slash
   return '/' + stack.join('/')
 }
