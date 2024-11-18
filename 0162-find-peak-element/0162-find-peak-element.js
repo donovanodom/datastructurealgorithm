683cb7dbc99bcb0d5ff77e5be3d7754d89c60d40
donovanodom <dodomdev@gmail.com>
2024-11-18 12:51:48 -0800

    Time: 0 ms (100.00%), Space: 49.2 MB (35.14%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -1,10 +1,10 @@
 const findPeakElement = (nums) => {
   let l = 0, r = nums.length - 1
-  while (l < r) {
-    const mid = Math.floor((l + r) / 2)
-    if (nums[mid + 1] > nums[mid]) {
+  while(l < r){
+    let mid = Math.floor((l + r) / 2)
+    if(nums[mid + 1] > nums[mid]){
       l = mid + 1
-    } else {
+    }else{
       r = mid
     }
   }
