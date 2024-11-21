a65d1df7a168a872e4060a8cb6913a484aee3ce8
donovanodom <dodomdev@gmail.com>
2024-11-20 21:07:31 -0800

    Time: 0 ms (100.00%), Space: 50.6 MB (57.58%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.js
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.js
@@ -1,17 +1,9 @@
-const sumNumbers = function(root, str = '', sum = 0) {
-  
-  // if no root return 0 as sum
+const sumNumbers = (root, str = '', sum = 0) => { 
   if(!root) return 0
-  
-  // append the current node val to the end string
   str += root.val
-  
-  // if a leaf is reached, add string as a number to the sum and return sum
   if(!root.left && !root.right){
     sum += Number(str)
     return sum
-  }
-  
-  // return the sum of traversing left and right down tree
+  } 
   return sumNumbers(root.left, str, sum) + sumNumbers(root.right, str, sum)
 }
