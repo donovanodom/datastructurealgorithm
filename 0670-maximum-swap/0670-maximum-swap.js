c971e4ca4ebb2da9b43f29f45ce2b1599376b325
donovanodom <dodomdev@gmail.com>
2024-11-21 16:55:29 -0800

    Time: 0 ms (100.00%), Space: 48.3 MB (95.41%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.js
@@ -0,0 +1,16 @@
+const maximumSwap = function(num) {
+   const arr = String(num).split('')
+   const dp = Array(9).fill(-1)
+   for(let i = 0; i < arr.length; i++){
+     dp[Number(arr[i])] = i
+   }
+  for(let i = 0; i < arr.length; i++){
+    for(let j = 9; j > arr[i]; j--){
+      if(dp[j] > i){
+        [arr[i], arr[dp[j]]] = [arr[dp[j]], arr[i]]
+        return Number(arr.join(''))
+      }
+    }
+  }
+  return num
+}
