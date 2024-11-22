7b2e3929d9d085e63e70333a29347e5f025386b5
donovanodom <dodomdev@gmail.com>
2024-11-21 18:06:13 -0800

    Time: 34 ms (15.66%) | Memory: 60.9 MB (23.90%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.js
+++ 498-diagonal-traverse/diagonal-traverse.js
@@ -1,25 +1,22 @@
 const findDiagonalOrder = function(mat) {
-  const res = [mat[0][0]], q = [[0,0,'up']], row = mat.length, col = mat[0].length
+  const res = [], q = [[0,0,'up']], row = mat.length, col = mat[0].length
   while(q.length){
     const [x, y, dir] = q.shift()
+    res.push(mat[x][y])
     if(dir == 'up'){
       if(x - 1 >= 0 && y + 1 < col){
-        res.push(mat[x - 1][y + 1])
         q.push([x - 1, y + 1, dir])
       }else{
         const [nX, nY, end] = reRoute([x,y], mat, dir)
         if(end) return res
-        res.push(mat[nX][nY])
         q.push([nX, nY, 'down'])
       }
     } else if (dir == 'down'){
       if(x + 1 < row && y - 1 >= 0){
-        res.push(mat[x + 1][y - 1])
         q.push([x + 1, y - 1, dir])
       }else{
         const [nX, nY, end] = reRoute([x,y], mat, dir)
         if(end) return res
-        res.push(mat[nX][nY])
         q.push([nX, nY, 'up'])
       }
     }
