2521ec2a061340437b79ccd3bcba8d10cc802d72
donovanodom <dodomdev@gmail.com>
2024-11-27 16:18:53 -0800

    Time: 0 ms (100.00%), Space: 48.6 MB (88.36%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.js
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.js
@@ -1,7 +1,7 @@
 const threeConsecutiveOdds = function(arr) {
   if(arr.length < 3) return false
-  for(let i = 0; i < arr.length - 2; i++){
-    if((arr[i] * arr[i+1] * arr[i+2]) % 2 != 0) return true
+  for(let i = 2; i < arr.length; i++){
+    if(arr[i] % 2 != 0 && arr[i - 1] % 2 != 0 && arr[i - 2] % 2 != 0) return true
   }
   return false
-};
+}
