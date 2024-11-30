10498d9800962ac949bbd72401e4dc1f5d10eda8
donovanodom <dodomdev@gmail.com>
2024-11-30 13:46:25 -0800

    Time: 76 ms (5.19%), Space: 59 MB (31.71%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.js
+++ 0986-interval-list-intersections/0986-interval-list-intersections.js
@@ -2,23 +2,23 @@ const intervalIntersection = function(firstList, secondList) {
   const res = []
   for(const first of firstList){
     for(const second of secondList){
-      if(checkIntersection(first, second)) res.push(createInterval(first, second))
+      const interval = createInterval(first, second)
+      if(interval) res.push(interval)
     }
   }
   return res
 }
 
-const checkIntersection = (first, second) => {
-  const [a1, b1] = first, [a2, b2] = second
-  if((a1 <= a2 && b1 >= a2) ||
-     (a1 >= a2 && a1 <= b2) ||
-     (a1 >= a2 && b1 <= b2) ||
-     (a2 >= a1 && b2 <= b1)
-    ) return true
-  return false
-}
-
 const createInterval = (first, second) => {
   const [a1, b1] = first, [a2, b2] = second
-  return [Math.max(a1, a2), Math.min(b1, b2)]
+  if(
+    (a1 <= a2 && b1 >= a2) ||
+    (a1 <= b2 && b1 >= b2) ||
+    (a1 <= a2 && b1 >= b2) ||
+    (a1 >= a2 && b1 <= b2)
+    ){
+    return [Math.max(a1, a2), Math.min(b1, b2)]
+  } 
 }
+
+  
