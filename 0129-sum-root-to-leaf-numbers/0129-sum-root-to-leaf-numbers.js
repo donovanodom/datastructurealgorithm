e241ff0509b9f19f89168405c1f6619c877633b8
donovanodom <dodomdev@gmail.com>
2024-11-30 12:57:54 -0800

    Time: 0 ms (100.00%), Space: 51 MB (26.43%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.js
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.js
@@ -1,9 +1,10 @@
 const sumNumbers = (root, str = '', sum = 0) => { 
-  if(!root) return 0
+  if(!root) return sum
   str += root.val
   if(!root.left && !root.right){
     sum += Number(str)
-    return sum
-  } 
-  return sumNumbers(root.left, str, sum) + sumNumbers(root.right, str, sum)
+  }else{
+    return sumNumbers(root.left, str, sum) + sumNumbers(root.right, str, sum)
+  }
+  return sum 
 }
