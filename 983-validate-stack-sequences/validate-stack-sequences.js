299d8ef850fcd1df62b911f145bbc1b54f4056d7
donovanodom <dodomdev@gmail.com>
2024-12-09 13:45:29 -0800

    Time: 3 ms (42.31%) | Memory: 51.2 MB (37.30%) - LeetSync

--- 983-validate-stack-sequences/validate-stack-sequences.js
+++ 983-validate-stack-sequences/validate-stack-sequences.js
@@ -1,12 +1,11 @@
 const validateStackSequences = function(pushed, popped) {
-  let popIndex = 0
+  let poppedIndex = 0
   const stack = []
-
-  for(const el of pushed){
-    stack.push(el) 
-    while(stack.length && popIndex <  popped.length && popped[popIndex] == stack[stack.length - 1]) {
-      popIndex++
+  for(const num of pushed){
+    stack.push(num)
+    while(poppedIndex < popped.length && popped[poppedIndex] == stack[stack.length - 1]){
       stack.pop()
+      poppedIndex++
     }
   }
   return !stack.length
