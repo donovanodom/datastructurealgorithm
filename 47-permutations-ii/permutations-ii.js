59217c212291dd69a8ecd15a55f1017d36c5163c
donovanodom <dodomdev@gmail.com>
2024-12-09 14:03:28 -0800

    Time: 167 ms (12.17%) | Memory: 58.5 MB (12.69%) - LeetSync

--- 47-permutations-ii/permutations-ii.js
+++ 47-permutations-ii/permutations-ii.js
@@ -1,24 +1,20 @@
 const permuteUnique = function(nums) {
-  const ans = [], seen = new Set()
-  const dfs = (i, nums) => {
+  const seen = []
+  const permute = function(nums, i = 0, ans = []) {
     if(i == nums.length){
-      const str = nums.slice().join(':')
-      if(!seen.has(str)){
-        seen.add(str)
-        ans.push(nums.slice())
+      const str = nums.join(':')
+      if(!seen.includes(str)){
+        seen.push(str)
+        ans.push([...nums])
       }
       return
     }
     for(let j = i; j < nums.length; j++){
-      const a = nums[i]
-      nums[i] = nums[j]
-      nums[j] = a
-      dfs(i + 1, nums)
-      const b = nums[i]
-      nums[i] = nums[j]
-      nums[j] = b
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      permute(nums, i + 1, ans);
+      [nums[i], nums[j]] = [nums[j], nums[i]];
     }
+    return ans
   }
-  dfs(0,nums)
-  return ans
-};
+  return permute(nums)
+}
