46d6b646ccd80c31860ad5dc01ce6e78589a5863
donovanodom <dodomdev@gmail.com>
2024-12-09 14:20:25 -0800

    Time: 133 ms (25.49%) | Memory: 73.6 MB (12.56%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -2,16 +2,9 @@ const findKthLargest = function(nums, k) {
   const map = {}
   for(const num of nums){
     let key = num + 10000
-    if(!map[key]) map[key] = 0
-    map[key]++
+    if(!map[key]) map[key] = []
+    map[key].push(num)
   }
-  let end = nums.length - k + 1, found = false, ans
-  Object.keys(map).map((key) => {
-    if(Number(map[key]) >= end && !found){
-      ans = Number(key) - 10000
-      found = true
-    } 
-    end -= map[key]
-  })
-  return ans
+  const arr = Object.values(map).flat()
+  return arr[arr.length - k]
 }
