e989ead57434358f801d73e813bf1a2fb13c86f1
donovanodom <dodomdev@gmail.com>
2024-12-09 16:09:01 -0800

    Time: 144 ms (5.24%), Space: 74.3 MB (5.04%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -1,15 +1,15 @@
 const validPalindrome = function(s) {
-    let i = 0, j = s.length - 1
-    while(i < j){
-        if(s[i] == s[j]){
-            i++
-            j--
-        }else{
-            let left = s.split(''), right = s.split('') 
-            left.splice(i,1)
-            right.splice(j,1)
-            return left.join('') == left.reverse().join('') || right.join('') == right.reverse().join('')
-        }
+  let i = 0, j = s.length - 1
+  while(i < j){
+    if(s[i] == s[j]){
+      i++
+      j--
+    }else{
+      const left = s.split(''), right = s.split('')
+      left.splice(i,1)
+      right.splice(j,1)
+      return left.join('') == left.reverse().join('') || right.join('') == right.reverse().join('')
     }
-    return true
+  }
+  return true
 }
