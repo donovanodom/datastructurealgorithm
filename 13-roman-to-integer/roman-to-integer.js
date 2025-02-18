9a83c253cf89d89539a3e4d3b54f348b6e87a79a
donovanodom <dodomdev@gmail.com>
2025-02-18 09:36:27 -0800

    Time: 9 ms (32.81%) | Memory: 60.9 MB (5.30%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.js
+++ 13-roman-to-integer/roman-to-integer.js
@@ -1,15 +1,15 @@
 const romanToInt = function(s) {
   const roman = ['I','V','X','L','C','D','M']
   const number = [1,5,10,50,100,500,1000]
-  let c = 0, ans = 0
-  while(c < s.length){
-    let l = roman.indexOf(s[c]), r = roman.indexOf(s[c+1])
-    if(r && number[l] < number[r]){
-      ans -= number[l]
-    }else{
-      ans += number[l]
+  let i = 0, res = 0
+  while(i < s.length){
+    const left = roman.indexOf(s[i]), right = roman.indexOf(s[i + 1])
+    if(right && number[left] < number[right]){
+      res -= number[left]
+    }else {
+      res += number[left]
     }
-    c++
+    i++
   }
-  return ans
+  return res
 };
