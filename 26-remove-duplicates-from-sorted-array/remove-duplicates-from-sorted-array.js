374d910a3f97c27f97c9664f2a3fcc3f682fdfc4
donovanodom <dodomdev@gmail.com>
2025-02-18 22:19:09 -0800

    Time: 0 ms (100.00%) | Memory: 59 MB (5.13%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -1,8 +1,10 @@
 const removeDuplicates = function(nums) {
   for(let i = 1; i < nums.length; i++){
-    while(nums[i] == nums[i - 1]){
-      nums.splice(i, 1)
+    let j = i
+    while(nums[j] == nums[i - 1]){
+      j++
     }
+    nums.splice(i, j - i)
   }
   return nums.length
 };
